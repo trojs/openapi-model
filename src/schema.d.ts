76f59685907f2224d85d8121200b44e0996b6827
Pieter Wigboldus <w3n@w3news.org>
2023-11-21 22:18:43 +0100

    Update the type interfaces

--- src/schema.d.ts
+++ src/schema.d.ts
@@ -30,3 +30,17 @@ export declare namespace OpenAPIV3 {
         example?: any;
     }
 }
+
+export interface Options {
+    validate?: boolean;
+}
+export class Model {
+    constructor(data?: object);
+    set data(data: object);
+    get data(): object;
+    valueOf(): any;
+}
+export function openapiToModel(
+    schema: BaseSchemaObject,
+    options: Options
+): Model;
